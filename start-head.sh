6550450d58f2de687ac093db62f6bea0336430f2
Mustafa Mustafa <mstftsm@gmail.com>
2021-01-15 18:31:02 -0800

    Change redis-port option to port; redis-port is deprecated

--- start-head.sh
+++ start-head.sh
@@ -5,5 +5,5 @@ export LANG=C.UTF-8
 
 echo "starting ray head node"
 # Launch the head node
-ray start --head --node-ip-address=$1 --redis-port=6379 --redis-password=$2
+ray start --head --node-ip-address=$1 --port=6379 --redis-password=$2
 sleep infinity
